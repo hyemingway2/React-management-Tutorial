a94c27571855d8d8ae5d42e0da22e53452cfe71b
hyemingway2 <hyemingwaypark@gmail.com>
2020-09-07 16:13:53 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 import Customer from './compoenets/Customer';
 import { render } from '@testing-library/react';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+
+
+const styles = {
+  root: {
+  width: "100%",
+  overflowX: "auto"
+  },
+  table: {
+  minWidth: 1080
+  }
+  };
 
 const customers = [
   {
@@ -34,12 +53,23 @@ const customers = [
 
 class App extends Component {
   render() {
+    const { classes }=this.props;
     return (
-      <div>
-        {
-
-          customers.map(c => {
-            return (
+      
+        <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>    
+            {customers.map(c => { return (
               <Customer
                 key={c.id}
                 id={c.id}
@@ -49,14 +79,13 @@ class App extends Component {
                 job={c.job}
               />
             );
-          })
+          })}</TableBody>
+        </Table>
+        </Paper>
+    
 
-
-
-        }
-      </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
